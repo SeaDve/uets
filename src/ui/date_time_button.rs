fad0f3e9df1df883050796d8b0c58627655cd8c8
Dave Patrick Caberto <davecruz48@gmail.com>
2024-11-12 21:48:06 +0800

    improve date picker

---
+++ src/ui/date_time_button.rs
@@ -0,0 +1,96 @@
+use gtk::{glib, prelude::*, subclass::prelude::*};
+
+use crate::{date_time_range::DateTimeRange, ui::date_time_window::DateTimeWindow};
+
+mod imp {
+    use std::cell::Cell;
+
+    use super::*;
+
+    #[derive(Default, glib::Properties, gtk::CompositeTemplate)]
+    #[properties(wrapper_type = super::DateTimeButton)]
+    #[template(resource = "/io/github/seadve/Uets/ui/date_time_button.ui")]
+    pub struct DateTimeButton {
+        #[property(get)]
+        pub(super) range: Cell<DateTimeRange>,
+
+        #[template_child]
+        pub(super) button: TemplateChild<gtk::Button>,
+        #[template_child]
+        pub(super) label: TemplateChild<gtk::Label>,
+    }
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for DateTimeButton {
+        const NAME: &'static str = "UetsDateTimeButton";
+        type Type = super::DateTimeButton;
+        type ParentType = gtk::Widget;
+
+        fn class_init(klass: &mut Self::Class) {
+            klass.bind_template();
+
+            klass.install_action_async("date-time-button.pick", None, |obj, _, _| async move {
+                let initial_range = if obj.range().is_all_time() {
+                    None
+                } else {
+                    Some(obj.range())
+                };
+
+                if let Ok(new_range) = DateTimeWindow::pick(initial_range, &obj).await {
+                    obj.set_range(new_range);
+                }
+            });
+        }
+
+        fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
+            obj.init_template();
+        }
+    }
+
+    #[glib::derived_properties]
+    impl ObjectImpl for DateTimeButton {
+        fn constructed(&self) {
+            self.parent_constructed();
+
+            let obj = self.obj();
+
+            obj.update_label();
+        }
+
+        fn dispose(&self) {
+            self.dispose_template();
+        }
+    }
+
+    impl WidgetImpl for DateTimeButton {}
+}
+
+glib::wrapper! {
+    pub struct DateTimeButton(ObjectSubclass<imp::DateTimeButton>)
+        @extends gtk::Widget;
+}
+
+impl DateTimeButton {
+    pub fn new() -> Self {
+        glib::Object::new()
+    }
+
+    fn set_range(&self, range: DateTimeRange) {
+        let imp = self.imp();
+
+        if range == self.range() {
+            return;
+        }
+
+        imp.range.set(range);
+        self.update_label();
+        self.notify_range();
+    }
+
+    fn update_label(&self) {
+        let imp = self.imp();
+
+        let range = self.range();
+        imp.label.set_label(&range.label_markup());
+    }
+}
